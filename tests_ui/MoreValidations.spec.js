c9ccb2a2834a6f0f3929651fe75abf7c71a84130
KathyPhanThi <dieuphan.it1@gmail.com>
2024-04-10 12:35:18 +0700

    Session 8

---
+++ tests_ui/MoreValidations.spec.js
@@ -0,0 +1,28 @@
+import { chromium, expect, test } from "@playwright/test";
+import exp from "constants";
+
+
+test.only("Popup validations", async({page}) => 
+{
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+    // await page.goto("https://google.com");
+    // await page.goBack();
+    // await page.goForward();
+    await expect(page.locator("#displayed-text")).toBeVisible();
+    await  page.locator("#hide-textbox").click();
+    await expect(page.locator("#displayed-text")).toBeHidden();
+    await  page.locator("#show-textbox").click();
+    await expect(page.locator("#displayed-text")).toBeVisible();
+    // click on popup
+    await page.pause();
+    page.on("dialog", dialog => dialog.accept());
+    await  page.locator("#confirmbtn").click();
+    await  page.locator("#mousehover").hover();
+
+    const framePage = page.frameLocator("#courses-iframe");
+ await framePage.locator("li a[href*='lifetime-access']:visible").click();
+ 
+ const textCheck = await framePage.locator(".text h2").textContent();
+ console.log(textCheck.split(" ")[1]);
+ 
+});
